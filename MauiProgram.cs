0b1aa3961ec3fb4a6bde67c291ec03b81780e969
m_rusinek <ruson80@gmail.com>
2022-07-17 16:38:48 +0200

    Checkpoint on configuration scaffolding.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,18 +1,26 @@
-﻿namespace Everyday.GUI;
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Everyday.GUI;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        var config = 
+            new ConfigurationBuilder()
+                .AddJsonStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Everyday.appsettings.json"))
+                    .Build();
+
+        return builder.Build();
+    }
 }
